2fa12ff536279bb074d5d3eaad21134710d7f4c7
sovietsus123 <susmeme174@gmail.com>
2023-09-11 19:43:54 +0700

    Lesson6

---
+++ Lesson6/firebase/index.js
@@ -0,0 +1,19 @@
+import { auth } from './config.js';
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+
+const welcometext = document.getElementById("welcome-text"); 
+
+const name = () => {
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const uid = user.uid;
+          welcometext.textContent = `Welcome ${user.email} to home page`;
+          // ...
+        } else {
+            alert("Please login first")
+            window.location = "./login.html";
+        }
+      });
+}
+name();

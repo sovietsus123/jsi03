8a4a5b191a88392f5df6640066a3b169c397fbf2
sds <susmeme174@gmail.com>
2023-10-08 14:13:53 +0700

    lesson

--- Test/script.js
+++ Test/script.js
@@ -1,63 +1,11 @@
-const users = [
-    {
-      id: 1,
-      name: "Leanne Graham",
-      address: {
-        street: "Kulas Light",
-        city: "Gwenborough",
-      },
-      company: {
-        name: "Vingroup",
-      },
-    },
-    {
-      id: 2,
-      name: "Ervin Howell",
-      address: {
-        street: "Victor Plains",
-        city: "McKenziehaven",
-      },
-      company: {
-        name: "MindX",
-      },
-    },
-    {
-      id: 3,
-      name: "Clementine Bauch",
-      address: {
-        street: "Douglas Extension",
-        city: "McKenziehaven",
-      },
-      company: {
-        name: "MindX",
-      },
-    },
-    {
-      id: 4,
-      name: "Patricia Lebsack",
-      address: {
-        street: "Hoeger Mall",
-        city: "South Elvis",
-      },
-      company: {
-        name: "Vingroup",
-      },
-    },
-  ];
 
-  const mindx = []
-  users.forEach((item) => {
-    if(item.company.name == "MindX"){
-        mindx.push(item.name)
+  let m = [1, 2, 3, 4, 5, 6, "hh", "9", 80, 100];
+  let squaredArr = [];
+  
+  for (let i = 0; i < m.length; i++) {
+    if (typeof m[i] === "number") {
+      squaredArr.push(m[i] * m[i]);
     }
-  });
-  console.log(mindx)
-
-  const vin = []
-
-  users.forEach((item) => {
-    if(item.company.name == "Vingroup"){
-        vin.push(item.name)
-    }
-  })
-  console.log(vin[1])
+  }
+  
+  console.log(squaredArr);
